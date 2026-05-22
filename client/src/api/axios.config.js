504a2dc3734972f0f4dd59beb754dd22c116c453
unknown <rizsameer001@gmail.com>
2026-05-22 14:16:59 +0530

    first commit

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,7 +1,8 @@
 // client/src/api/axios.config.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api/v1';
+//const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
